6f3b769f122f120633bbc71d6d082cfed91f47ba
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-31 08:46:49 -0300

    fazendo inw

--- revac2mia.js
+++ revac2mia.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 //configurando o roteamento para teste no postman
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const port = 3000;
-
 
 //configurando o acesso ao mongodb
 mongoose.connect('mongodb://127.0.0.1:27017/revac2mia',
@@ -17,7 +15,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/revac2mia',
     useUnifiedTopology: true,
     serverSelectionTimeoutMS : 20000
 });
-
 
 //criando a model do seu projeto
 const PessoaSchema = new mongoose.Schema({
@@ -28,7 +25,6 @@ const PessoaSchema = new mongoose.Schema({
     cep : {type : String, required : true},
     nascimento : {type : Date, required : true}
 });
-
 
 const Pessoa = mongoose.model("Pessoa", PessoaSchema);
 
@@ -42,7 +38,6 @@ app.post("/cadastropessoa", async(req, res)=>{
     const cep  = req.body.cep;
     const nascimento = req.body.nascimento
 
-
     const pessoa = new Pessoa({
         nome : nome,
         email : email,
@@ -52,22 +47,23 @@ app.post("/cadastropessoa", async(req, res)=>{
         nascimento : nascimento
     })
 
-
     try{
         const newPessoa = await pessoa.save();
         res.json({error : null, msg : "Cadastro ok", pessoaId : newPessoa._id});
     } catch(error){
         res.status(400).json({error});
     }
-
-
 });
 
+//rota para o get de cadastro
+app.get("/cadastropessoa.html", async(req, res)=>{
+    res.sendFile(__dirname +"/cadastropessoa.html");
+})
 
+//rota raiz - inw
 app.get("/", async(req, res)=>{
     res.sendFile(__dirname +"/index.html");
 })
-
 
 //configurando a porta
 app.listen(port, ()=>{
